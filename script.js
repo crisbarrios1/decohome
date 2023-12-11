cd48892b447dc14aacf5fa39bcc0236468230694
crisBarrios <cristian.leny@gmail.com>
2023-12-11 18:43:02 -0300

    Corrección de comentarios

--- script.js
+++ script.js
@@ -23,95 +23,77 @@ async function handleSubmit(event){
 }
 
 
-// Crea una carpeta “js” y un archivo “productos.js”
-const { createApp } = Vue
-    createApp({
-        data() {
-            return { productos:[],
-                    //url:'http://localhost:5000/productos',
-                    // si el backend esta corriendo local usar localhost 5000(si no lo subieron a pythonanywhere)
-                    url:'http://mcerda.pythonanywhere.com/productos', // si ya lo subieron a pythonanywhere
-                    error:false,
-                    cargando:true,
-                    /*atributos para el guardar los valores del formulario */
-                    id:0,
-                    nombre:"",
-                    imagen:"",
-                    stock:0,
-                    precio:0,
-                    }
-                },
-                    methods: {
-                    fetchData(url) {
-                    fetch(url)
-                    .then(response => response.json())
-                    .then(data => {
-                    this.productos = data;
-                    this.cargando=false
-                    })
-                    .catch(err => {
-                    console.error(err);
-                    this.error=true
-                    })
-                    },
-                    eliminar(producto) {
-                    const url = this.url+'/' + producto;
-                    var options = {
-                    method: 'DELETE',
-                    }
-                    fetch(url, options)
-                    .then(res => res.text()) // or res.json()
-                    .then(res => {
-                    location.reload();
-                    })
-                    },
-                    grabar(){
-                    let producto = {
-                    nombre:this.nombre,
-                    precio: this.precio,
-                    stock: this.stock,
-                    imagen:this.imagen
-                    }
-                    var options = {
-                    body:JSON.stringify(producto),
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    redirect: 'follow'
-                    }
-                    fetch(this.url, options)
-                    .then(function () {
-                    alert("Registro grabado")
-                    window.location.href = "./productos.html";
-                    })
-                    .catch(err => {
-                    console.error(err);
-                    alert("Error al Grabarr")
+// // Crea una carpeta “js” y un archivo “productos.js”
+// const { createApp } = Vue
+//     createApp({
+//         data() {
+//             return { productos:[],
+                   
+//                     url:'http://mcerda.pythonanywhere.com/productos', 
+//                     cargando:true,
+//                     /*atributos para el guardar los valores del formulario */
+//                     id:0,
+//                     nombre:"",
+//                     imagen:"",
+//                     stock:0,
+//                     precio:0,
+//                     }
+//                 },
+//                     methods: {
+//                     fetchData(url) {
+//                     fetch(url)
+//                     .then(response => response.json())
+//                     .then(data => {
+//                     this.productos = data;
+//                     this.cargando=false
+//                     })
+//                     .catch(err => {
+//                     console.error(err);
+//                     this.error=true
+//                     })
+//                     },
+//                     eliminar(producto) {
+//                     const url = this.url+'/' + producto;
+//                     var options = {
+//                     method: 'DELETE',
+//                     }
+//                     fetch(url, options)
+//                     .then(res => res.text()) // or res.json()
+//                     .then(res => {
+//                     location.reload();
+//                     })
+//                     },
+//                     grabar(){
+//                     let producto = {
+//                     nombre:this.nombre,
+//                     precio: this.precio,
+//                     stock: this.stock,
+//                     imagen:this.imagen
+//                     }
+//                     var options = {
+//                     body:JSON.stringify(producto),
+//                     method: 'POST',
+//                     headers: { 'Content-Type': 'application/json' },
+//                     redirect: 'follow'
+//                     }
+//                     fetch(this.url, options)
+//                     .then(function () {
+//                     alert("Registro grabado")
+//                     window.location.href = "./productos.html";
+//                     })
+//                     .catch(err => {
+//                     console.error(err);
+//                     alert("Error al Grabarr")
 
-                    })
-                    }
-                    },
-                created() {
-                this.fetchData(this.url)
-                },
-                }).mount('#app')
+//                     })
+//                     }
+//                     },
+//                 created() {
+//                 this.fetchData(this.url)
+//                 },
+//                 }).mount('#app')
 
 
 
-function admin () {
-    pass = document.getElementById("pass").value;
-    if (pass !== null) {
-        // Tomar decisiones basadas en el valor ingresado
-        if (pass == "admin123") {
-            alert("Contraseña correcta");
-            window.location.href = 'productos.html';
-        } else {
-            alert("Contraseña incorrecta.");
-            window.location.href = 'index.html'
-        }
-        } else {
-        // El usuario presionó Cancelar en el prompt
-        alert("Operación cancelada, no ingresaste ninguna clave.");
-        window.location.href = 'index.html'
-        }
-    }
+
                 
